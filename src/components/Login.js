701e233a32c40994ff39ba5cef17d3fae19a0fad
djonata-dias <diasdjonata@gmail.com>
2020-07-15 18:50:58 -0300

    validaçao de PropTypes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { onChange } from '../action';
 import Input from './ultilityComponents/Input';
 import Button from './ultilityComponents/Button';
-import { onChange } from '../action';
-import { Link } from 'react-router-dom';
 import configIcon from '../images/config-icon.png';
 
-const Login = ({ onChangeProps, name, email }) => {
+const Login = ( onChangeProps, name, email ) => {
   return (
     <div>
       <Input
@@ -19,14 +20,18 @@ const Login = ({ onChangeProps, name, email }) => {
         name="email"
         test="input-gravatar-email"
       />
-      <Link to="/game">
-        <Button isDisabled={!(name && email)} test="btn-play" name="Jogar" />
-      </Link>
-      <Link to="/settings"test="btn-settings" name="Configurações">
-        <img src={configIcon}  alt="ícone de engrenagem que redireciona para a pagina de configurações"/>
+      <Link to="/game"><Button isDisabled={!(name && email)} test="btn-play" name="Jogar" /></Link>
+      <Link to="/settings" test="btn-settings" name="Configurações">
+        <img src={configIcon} alt="ícone de engrenagem" width="40px" />
       </Link>
     </div>
   );
+};
+
+Login.propTypes = {
+  onChangeProps: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
 const mapState = (state) => ({
