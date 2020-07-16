cee66cec5cc75667c4e123e9fa978082a01f4231
djonata-dias <diasdjonata@gmail.com>
2020-07-16 16:00:29 -0300

    criei a pagina de configuraçoes

---
+++ src/pages/SettingsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1 data-testid="settings-title" >Settings</h1>
+    </div>
+  );
+};
+
+export default SettingsPage;
