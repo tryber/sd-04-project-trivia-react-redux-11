c112df97f4c8106758e07c771e9ed972164bdeea
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-16 19:00:38 -0300

    Feedbacks, feedbackanswers nao é chamado pq?

---
+++ src/components/FeedBack.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Header from './Header';
+import FeedbackAnswears from './FeedbackAnswears'; // porque nao chama??
+
+class FeedbackAnswears extends React.Component {
+  restartGame() {
+    const { clearlogin } = this.props;
+    clearlogin();
+    // criar na action
+  }
+
+  newGame() {
+    const { clearpoints } = this.props;
+    clearpoints();
+    // criar na action
+  }
+
+  tryAgain() {
+    return (
+      <Link to="/">
+        <button type="button" onClick={() => this.newGame()}>Tentar novamente</button>
+      </Link>
+    );
+  }
+
+  render() {
+    const { totalAnswears, scorePoints } = this.props;
+    return (
+      <div>
+        <Header />
+        <FeedbackAnswears totalAnswears={totalAnswears} />
+        <div>
+          <span>Você acertou</span>
+          <span data-testid="feedback-total-question">{totalAnswears}</span>
+          <span>questões</span>
+        </div>
+        <div>
+          <span>Fez um total de</span>
+          <span data-testid="feedback-total-score">{scorePoints}</span>
+          <span>pontos</span>
+        </div>
+        {this.tryAgain()}
+        <Link to="/">
+          <button type="button" onClick={() => this.restartGame()} data-testid="btn-play-again">
+            Jogar novamente
+          </button>
+        </Link>
+        <Link to="/"> 
+          <button type="button" onClick={() => this.restartGame()} data-testid="btn-ranking">
+            Ver Ranking
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
