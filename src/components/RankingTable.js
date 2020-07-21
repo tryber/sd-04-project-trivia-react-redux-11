fcd964a825161aaaf247b52faf3617f5b2420f0e
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-21 20:53:33 -0300

    CC 32

--- src/components/RankingTable.js
+++ src/components/RankingTable.js
@@ -6,22 +6,22 @@ import Image from './ultilityComponents/Image';
 import Button from './ultilityComponents/Button';
 
 const RankingTable = ({ ranking }) => (
-    <div>
-      <table>
-        { ranking.map(({ name, score, picture }, index) => (
-          <tr>
-            <td data-testid={`player-name-${index}`}>{name}</td>
-            <td data-testid={`player-score-${index}`}>{score}</td>
-            <td>
-              <Image src={`https://www.gravatar.com/avatar/${picture}`} />
-            </td>
-          </tr>
-        )) }
-      </table>
-      <Link to="/">
-        <Button test="btn-go-home" name="jogar-novamente" />
-      </Link>
-    </div>
+  <div>
+    <table>
+      {ranking.map(({ name, score, picture }, index) => (
+        <tr>
+          <td data-testid={`player-name-${index}`}>{name}</td>
+          <td data-testid={`player-score-${index}`}>{score}</td>
+          <td>
+            <Image src={`https://www.gravatar.com/avatar/${picture}`} />
+          </td>
+        </tr>
+      ))}
+    </table>
+    <Link to="/">
+      <Button test="btn-go-home" name="jogar-novamente" />
+    </Link>
+  </div>
 );
 const mapState = (state) => ({
   ranking: state.answers.ranking,
