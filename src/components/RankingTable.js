1d1a3c8c3b08627f99bba4711b1314bace3f69a4
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-21 20:29:10 -0300

    CC

--- src/components/RankingTable.js
+++ src/components/RankingTable.js
@@ -4,29 +4,29 @@ import { Link } from 'react-router-dom';
 import Image from './ultilityComponents/Image';
 import Button from './ultilityComponents/Button';
 
-const RankingTable = ({ranking}) => {
+const RankingTable = ({ ranking }) => {
   return (
     <div>
       <table>
-        {ranking.map(({name, score, picture}, index) => (
-          <tr>  
+        { ranking.map(({ name, score, picture }, index) => (
+          <tr>
             <td data-testid={`player-name-${index}`}>{name}</td>
             <td data-testid={`player-score-${index}`}>{score}</td>
             <td>
               <Image src={`https://www.gravatar.com/avatar/${picture}`} />
             </td>
           </tr>
-      ))}
+        ))}
       </table>
-        <Link to="/">
-        <Button test="btn-go-home" name="jogar-novamente" /> 
-        </Link>
+      <Link to="/">
+        <Button test="btn-go-home" name="jogar-novamente" />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 const mapState = (state) => ({
-  ranking: state.answers.ranking
-})
+  ranking: state.answers.ranking,
+});
 
 export default connect(mapState)(RankingTable);
